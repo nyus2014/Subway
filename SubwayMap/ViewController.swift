963dc89437b9603b190d6a6b64acfec29c89c0d2
Sihang Huang <sihanghuang@Sihangs-MacBook-Pro.local>
2014-10-29 13:35:21 -0400

    Initial Commit

---
+++ SubwayMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubwayMap
+//
+//  Created by Sihang Huang on 10/29/14.
+//  Copyright (c) 2014 Sihang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
